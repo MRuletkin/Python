11bdd678d3d58ec661d12fed852eb6d13d3a2a16
MRuletkin <62499749+mruletkin@users.noreply.github.com>
2020-09-24 19:23:44 +0300

    Initial commit

---
+++ Litvinov_task.py
@@ -0,0 +1,41 @@
+######################### 1)
+# value = int(input('Введите число: '))
+# new_value = value / 2 if value < 100 else value * (-1)
+# print(new_value)
+######################### 2)
+# value = int(input('Введите число: '))
+# new_value = 1 if value < 100 else 0
+# print(new_value)
+######################### 3)
+# value = int(input('Введите число: '))
+# new_value = value < 100 if value < 100 else value < 100
+# print(new_value)
+######################## 4)
+# my_str = 'ArDuiNo'
+# a = ''
+# for i in my_str:
+#     a += i.upper() if i.islower() else i
+# print(a)
+# ####################### 5)
+# my_str = 'ArDuiNo'
+# a = ''
+# for i in my_str:
+#     a += i.lower() if not i.islower() else i
+# print(a)
+#
+# print(my_str.upper())
+######################### 6)
+# my_str = input('Введите что-нибудь: ')
+# if len(my_str) < 5:
+#     print(my_str * 2)
+# else:
+#     print(my_str)
+######################## 7)
+# my_str = input('Введите что-нибудь: ')
+# if len(my_str) < 5:
+#     print(my_str + my_str[:-1])
+# else:
+#     print(my_str)
+print()
+
+

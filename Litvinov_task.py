a2d2607eac74e66b7d6e76529fc253575b0916e9
Mruletkin <mruletkin6@gmail.com>
2020-09-25 23:53:37 +0300

    Initial commit

--- Litvinov_task.py
+++ Litvinov_task.py
@@ -2,15 +2,15 @@
 # value = int(input('Введите число: '))
 # new_value = value / 2 if value < 100 else value * (-1)
 # print(new_value)
-######################### 2)
+######################## 2)
 # value = int(input('Введите число: '))
 # new_value = 1 if value < 100 else 0
 # print(new_value)
-######################### 3)
+# ######################## 3)
 # value = int(input('Введите число: '))
 # new_value = value < 100 if value < 100 else value < 100
 # print(new_value)
-######################## 4)
+# ####################### 4)
 # my_str = 'ArDuiNo'
 # a = ''
 # for i in my_str:
@@ -24,18 +24,34 @@
 # print(a)
 #
 # print(my_str.upper())
-######################### 6)
+# ######################## 6)
 # my_str = input('Введите что-нибудь: ')
 # if len(my_str) < 5:
 #     print(my_str * 2)
 # else:
 #     print(my_str)
-######################## 7)
+# ####################### 7)
 # my_str = input('Введите что-нибудь: ')
 # if len(my_str) < 5:
 #     print(my_str + my_str[:-1])
 # else:
 #     print(my_str)
-print()
+####################### 8)
+# my_str = 'P ! t h 0 n @'
+# for i in my_str:
+#     if i.isalnum():
+#         print(i)
+####################### 9)
+# my_str = 'P ! t h 0 n @'
+# for i in my_str:
+#     if not i.isalnum():
+#         print(i)
+###################### 10)
+my_str = 'P ! t h 0 n @'
+for i in my_str:
+    if not i.isspace() and not i.isalnum():
+        print(i)
+
+
 
 

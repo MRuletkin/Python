cfaf0e64c6bd1468d3daf4664e22fcb5597b2895
Mruletkin <mruletkin6@gmail.com>
2020-10-11 11:46:04 +0300

    Initial commit

--- Litvinov_task_7.py
+++ Litvinov_task_7.py
@@ -9,13 +9,13 @@ from random import randint
 # print(my_list)
 #
 # ##################### 2)
-triangle = {'A': (),
-            'B': (),
-            'C': ()}
-for key in triangle:
-    triangle[key] = tuple([randint(1, 10) for _ in range(3)])
-
-print(triangle)
+# triangle = {'A': (),
+#             'B': (),
+#             'C': ()}
+# for key in triangle:
+#     triangle[key] = tuple([randint(1, 10) for _ in range(3)])
+#
+# print(triangle)
 #
 # ################### 3)
 # my_string = 'qwerty'
@@ -26,10 +26,16 @@ print(triangle)
 # print(my_print(my_string))
 
 ################### 4)
-my_dict_1 = {f"key_{a}": a ** 2 for a in range(7)}
-my_dict_2 = {f"key_{b}": b ** 2 for b in range(2,6)}
+my_dict_1 = {f'key_{a}': a ** 2 for a in range(7)}
+my_dict_2 = {f'key_{b}': b ** 2 for b in range(2, 6)}
+# print(my_dict_1)
+# print(my_dict_2)
 #
 # ### a)
+# my_set_1 = set(my_dict_1)
+# my_set_2 = set(my_dict_2)
+# my_list = [*my_set_1.intersection(my_set_2)]
+# print(my_list)
 # new_list = [key for key in my_dict_1 if key in my_dict_2]
 # print(new_list)
 #
@@ -38,10 +44,17 @@ my_dict_2 = {f"key_{b}": b ** 2 for b in range(2,6)}
 # print(new_list)
 
 ### c)
-# new_dict = {}
-# for key in my_dict_1:
-#     if key in my_dict_2:
-#         print(key, my_dict_1[key]
+# new_dict = {f'key_{key}': my_dict_1[key] for key in my_dict_1 if key not in my_dict_2}
+# print(new_dict)
+
+### d)
+
+my_dict_2['key_7'] = 50
+my_set = list(set(my_dict_1).symmetric_difference(my_dict_2))
+
+
+
+
 
 
 

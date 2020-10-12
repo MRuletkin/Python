1963117149829b9135962797bbee897be02c3b64
Mruletkin <mruletkin6@gmail.com>
2020-10-12 21:18:34 +0300

    Initial commit

--- Litvinov_task_7.py
+++ Litvinov_task_7.py
@@ -1,7 +1,7 @@
 ################ 1)
 from random import randint
-# my_list = [randint(1, 100) for _ in range(20)]
-# print(my_list)
+my_list = [randint(1, 100) for _ in range(20)]
+print(my_list)
 #
 # my_list = []
 # for _ in range(20):
@@ -9,54 +9,49 @@ from random import randint
 # print(my_list)
 #
 # ##################### 2)
-# triangle = {'A': (),
-#             'B': (),
-#             'C': ()}
-# for key in triangle:
-#     triangle[key] = tuple([randint(1, 10) for _ in range(3)])
-#
-# print(triangle)
+triangle = {'A': (),
+            'B': (),
+            'C': ()}
+for key in triangle:
+    triangle[key] = tuple([randint(1, 10) for _ in range(3)])
+
+print(triangle)
 #
 # ################### 3)
-# my_string = 'qwerty'
-# def my_print(string):
-#     new_string = '***' + string + '***'
-#     return new_string
-#
-# print(my_print(my_string))
+my_string = 'qwerty'
+def my_print(string):
+    new_string = '***' + string + '***'
+    return new_string
+
+print(my_print(my_string))
 
 ################### 4)
 my_dict_1 = {f'key_{a}': a ** 2 for a in range(7)}
 my_dict_2 = {f'key_{b}': b ** 3 for b in range(2, 6)}
-# print(my_dict_1)
-# print(my_dict_2)
 #
 # ### a)
-# my_set_1 = set(my_dict_1)
-# my_set_2 = set(my_dict_2)
-# my_list = [*my_set_1.intersection(my_set_2)]
-# print(my_list)
+my_list = [*set(my_dict_1).intersection(set(my_dict_2))]
+print(my_list)
 # new_list = [key for key in my_dict_1 if key in my_dict_2]
 # print(new_list)
 #
-# ### b)
+# ### б)
+my_list = [*set(my_dict_1).difference(set(my_dict_2))]
+print(my_list)
 # new_list = [key for key in my_dict_1 if key not in my_dict_2]
 # print(new_list)
 
-### c)
-# new_dict = {f'key_{key}': my_dict_1[key] for key in my_dict_1 if key not in my_dict_2}
-# print(new_dict)
+### в)
+new_dict = {f'{key}': my_dict_1[key] for key in my_dict_1 if key not in my_dict_2}
+print(new_dict)
 
-### d)
+### г)
 my_dict_2['key_7'] = 50
 new_dict = {}
-my_set_1 = set(my_dict_1.keys()).symmetric_difference(my_dict_2.keys())
-my_set_2 = set(my_dict_1.keys()).intersection(my_dict_2.keys())
 union_dict = {**my_dict_1, **my_dict_2}
-
-for key in my_set_1:
-    new_dict[key] = union_dict[key]
-for key in my_set_2:
+for key in set(my_dict_1.keys()).symmetric_difference(my_dict_2.keys()):
+    new_dict.update({key: union_dict.get(key)})
+for key in set(my_dict_1.keys()).intersection(my_dict_2.keys()):
     new_dict[key] = my_dict_1[key], my_dict_2[key]
 
 print(new_dict)
--- Litvinov_task_8.py
+++ Litvinov_task_8.py
@@ -1,1 +1,54 @@
-import
+############# 1)
+def domains():
+    domains = []
+    open_file = open('domains.txt', 'r')
+    for line in open_file.readlines():
+        domen = line.strip().replace('.', '')
+        domains.append(domen)
+    return domains
+
+list_domains = domains()
+# print(list_domains)
+
+############# 2)
+def names():
+    nationality = ['English', 'Spanish', 'Welsh', 'Irish', 'Scottish']
+    new_list = []
+    open_file = open('names.txt', 'r')
+    for line in open_file.readlines():
+        my_line = line.split()
+        for line in my_line:
+            if line not in nationality and line.isalpha():
+                new_list.append(line)
+    return new_list
+
+list_names = names()
+# print(list_names)
+
+############## 3)
+import random
+
+def get_name():
+    return random.choice(list_names)
+
+def get_num():
+    return str(random.randint(100, 999))
+
+def get_letters():
+    my_alphabet = []
+    for index in range(ord('a'), ord('z') + 1):
+        my_alphabet += chr(index)
+    return ''.join(random.choice(my_alphabet) for i in range(random.randint(5, 7)))
+
+# a = get_letter()
+# print(a)
+
+def get_domain():
+    return random.choice(list_domains)
+
+def get_email():
+    return get_name() + '.' + get_num() + '@' + get_letters() + '.' + get_domain()
+a = get_email()
+print(a)
+
+
